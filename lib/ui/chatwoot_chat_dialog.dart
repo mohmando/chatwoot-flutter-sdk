5ec2138d2ff7e2e88b422d868a4423c1ce651967
Ephraim Nartey <enartey@adiutor.co>
2024-12-03 10:50:20 +0000

    fix(chatwoot_chat_dialog): remove ununsed imports

--- lib/ui/chatwoot_chat_dialog.dart
+++ lib/ui/chatwoot_chat_dialog.dart
@@ -2,8 +2,6 @@ import 'package:chatwoot_sdk/data/local/entity/chatwoot_user.dart';
 import 'package:chatwoot_sdk/ui/chatwoot_chat_theme.dart';
 import 'package:chatwoot_sdk/ui/chatwoot_l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:intl/intl.dart';
 
 import 'chatwoot_chat_page.dart';
 
